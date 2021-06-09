bd454fd60856687c9375ae7bcff779697ab29f58
ralphielega <69807998+ralphielega@users.noreply.github.com>
2021-06-09 09:21:59 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,72 @@
+$(".arm").dblclick(function() {
+   $(".kidney").show();
+});
+ $(".leave").click(function() {
+   $(".leg").show();
+});
+$(".arm").dblclick(function() {
+   $(".caillou").hide();
+    });
+$(".leave").click(function() {
+   $(".caillou").hide();
+});
+$(".arm").dblclick(function() {
+   $(".stone").show();
+});
+$(".arm").dblclick(function() {
+   $(".ImageOne").hide();
+});
+$(".leave").click(function() {
+   $(".potato").show();
+});
+$(".leave").click(function() {
+   $(".ImageOne").hide();
+});
+$(".leave").click(function() {
+   $(".leave").hide();
+});
+$(".arm").dblclick(function() {
+   $(".arm").hide();
+});
+$(".arm").dblclick(function() {
+   $(".toe").show();
+});
+$(".arm").dblclick(function() {
+   $(".leave").hide();
+});
+$(".toe").dblclick(function() {
+   $(".rat").show();
+});
+$(".toe").dblclick(function() {
+   $(".toe").hide();
+});
+$(".toe").dblclick(function() {
+   $(".kidney").hide();
+});
+$(".toe").dblclick(function() {
+   $(".stone").hide();
+});
+$(".leave").click(function() {
+   $(".arm").hide();
+});
+$(".leave").click(function() {
+   $(".mice").show();
+});
+$(".mice").click(function() {
+   $(".chicken").show();
+});
+$(".mice").click(function() {
+   $(".leg").hide();
+});
+$(".mice").click(function() {
+   $(".potato").hide();
+});
+$(".mice").dblclick(function() {
+   $(".chicken").hide();
+});
+$(".mice").dblclick(function() {
+   $(".fun").show();
+});
+$(".mice").dblclick(function() {
+   $(".mice").hide();
+});
